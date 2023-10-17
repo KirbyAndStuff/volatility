e6878729a271ce59457f9abd8d3f97c6d445c032
KirbyAndStuff <nguungoku@gmail.com>
2023-10-17 14:32:07 +0200

    made red have a dash to attack the player

--- particle/enemies/red.gd
+++ particle/enemies/red.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var particlesL: CPUParticles2D
 var particlesR: CPUParticles2D
 var plred_death := preload("res://enemies/red_death.tscn")
+var red_dash_particles := preload("res://enemies/red_dash_particles.tscn")
 
 func _ready():
 	particlesL = $"left eye"
@@ -12,6 +13,10 @@ var speed = 300
 var player_chase = false
 var player = null
 var attack_player = false
+var dash_at_player = false
+var red_stamina = 20
+
+@onready var red_dashlength = $Red_DashLength
 
 func _physics_process(delta):
 	if player_chase:
@@ -23,6 +28,10 @@ func _process(delta):
 	if attack_player and (get_node("../player").i_frames) == false and (get_node("../player").dashi_frames) == false:
 		(get_node("../player").health) -= 1
 		(get_node("../player").i_frames) = true
+	if red_stamina <= 20:
+		red_stamina += 10 * delta
+	if dash_at_player == true:
+		speed = 800
 
 func _on_playerdeath_body_entered(body):
 	if body.name == "bullet4":
@@ -54,3 +63,29 @@ func _on_hurts_player_body_entered(body):
 func _on_hurts_player_body_exited(body):
 	if body.name == "player":
 		attack_player = false
+		
+
+func _on_player_dash_distance_body_entered(body):
+	if body.name == "player" and red_stamina > 19:
+		red_stamina -= 20
+		var effect := red_dash_particles.instantiate()
+		effect.position = position
+		get_parent().add_child(effect)
+		await get_tree().create_timer(0.5).timeout
+		dash_at_player = true
+		red_dashlength.start()
+		
+func _on_player_dash_distance_body_exited(body):
+	if body.name == "player" and red_stamina > 19:
+		red_stamina -= 20
+		var effect := red_dash_particles.instantiate()
+		effect.position = position
+		get_parent().add_child(effect)
+		await get_tree().create_timer(0.5).timeout
+		dash_at_player = true
+		red_dashlength.start()
+		
+
+func _on_red_dash_length_timeout() -> void:
+	speed = 300
+	dash_at_player = false
